e3fb8da842a1e064a21596c696e3a120f386b197
Blake Lawson <blakemlaws@gmail.com>
2014-11-15 16:43:11 -0500

    Made basic structure for page displaying images

--- docker/manager/urls.py
+++ docker/manager/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^create/$', views.create_image, name='create'),
     url(r'^status/$', views.display_instances, name='status'),
+    url(r'^images/$', views.display_images, name='images'),
 )
