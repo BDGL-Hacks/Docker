90c6ce45329b653f9483213242b9a94cdc47ef73
Lance Goodridge <ldgoodridge95@gmail.com>
2014-11-15 14:55:32 -0500

    Added start_container method to utils.py
    Added start_container method, which starts a container given
    an image and a bunch of different options. Has not been
    extensively tested yet.

--- docker/manager/utils.py
+++ docker/manager/utils.py
@@ -1,6 +1,7 @@
 '''
 Contains scripts for manipulating docker data/commmands.
 '''
+import docker
 import json
 import re
 import StringIO
@@ -10,26 +11,73 @@ from subprocess32 import Popen, PIPE
 Returns the output of 'docker ps' as a JSON
 '''
 def get_status():
-	# command = "docker ps"
-	command = "sudo docker ps"		# Make sure to update so not using sudo
+    # command = "docker ps"
+    command = "sudo docker ps"		# Make sure to update so not using sudo
 
-	# Get output from command
-	p = Popen(command.split(), stdout=PIPE)
-	out, err = p.communicate()
+    # Get output from command
+    p = Popen(command.split(), stdout=PIPE)
+    out, err = p.communicate()
 
-	separator_pattern = re.compile(r"\s\s+")
+    separator_pattern = re.compile(r"\s\s+")
 
-	# parse output from command and make structure for json
-	s = StringIO.StringIO(out)
-	headings = separator_pattern.split(s.readline())
-	container = { headings[0]: {} }
-	for line in s.readlines():
-		l = separator_pattern.split(line)
-		container[headings[0]][l[0]] = {}
-		current = container[headings[0]][l[0]]
-		for i in range(1, len(l)):
-			current[headings[i]] = l[i]
+    # parse output from command and make structure for json
+    s = StringIO.StringIO(out)
+    headings = separator_pattern.split(s.readline())
+    container = { headings[0]: {} }
+    for line in s.readlines():
+        l = separator_pattern.split(line)
+        container[headings[0]][l[0]] = {}
+        current = container[headings[0]][l[0]]
+        for i in range(1, len(l)):
+            current[headings[i]] = l[i]
 
-	json_base = [container]
+    json_base = [container]
 
-	return json.dumps(json_base)
+    return json.dumps(json_base)
+
+'''
+Starts a docker container with the provided image and options.
+
+Parameters: 
+container_name  =   (string) Name of the container
+image_name      =   (string) Name of the image to run
+links           =   (string list) A list of containers to link to
+host_mounts     =   (string:string dictionary) A dictionary linking paths to volumes
+                    on the host computer to their mount destination in the container
+external_mounts =   (string list) A list of containers to mount from
+custom_mounts   =   (string list) A list of paths for volumes to create and mount
+is_interactive  =   (bool) Specifies whether or not you want terminal access
+is_background   =   (bool) Specifies whether the container should run in the background
+
+Returns the response from the client start method
+'''
+def start_container(container_name, image_name, links, host_mounts, external_mounts,
+    custom_mounts, is_interactive, is_background):
+    
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', verion='1.9', timeout=10)
+
+    # Create a list of all volume paths to mount (not incuding those from other containers)
+    all_volumes = host_mounts + custom_mounts
+
+    # Convert the links argument into a dictionary
+    links_dict = {}
+    for link in links:
+        links_dict[link] = link
+
+    # Create the volume mount binds dictionary
+    binds_dict = {}
+    for host_path in host_mounts:
+        dest_path = host_mounts[host_path]
+        binds_dict[host_path] = {'bind' : dest_path, 'ro' : False}
+
+    # Create a container and get its id
+    container = client.create_container(image=image_name, name=container_name, 
+        volumes=all_volumes, volumes_from=external_mounts, tty=is_interactive,
+        stdin_open=is_interactive, detach=is_background)
+    containerID = container.get('Id')
+
+    # Start the container and return the response
+    response = client.start(container=container.get('Id'), publish_all_ports=True, 
+        links=links_dict, binds=binds_dict)
+    return response
