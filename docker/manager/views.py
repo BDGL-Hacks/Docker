597213b749082153891268131a664b62ed8fa0c0
Blake Lawson <blakemlaws@gmail.com>
2014-11-15 01:03:53 -0500

    Success working with forms

--- docker/manager/views.py
+++ docker/manager/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from forms import CreateInstForm
 
 # Create your views here.
 def home(request):
 	return render(request, 'manager/main_panel.html')
 
 def create_image(request):
-	return render(request, 'manager/create_instance.html')
+	if request.method == 'GET':
+		form = CreateInstForm(request.GET)
+		if form.is_valid():
+			# Create new containers with given parameters
+			return HttpResponseRedirect('/')
+	else:
+		form = CreateInstForm()
+
+	return render(request, 'manager/create_instance.html', { 'form': form })
