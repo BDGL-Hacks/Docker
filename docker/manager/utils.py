f96a9f0cf80233d0184565f8c5142eda84b90ac7
Lance Goodridge <ldgoodridge95@gmail.com>
2014-11-16 01:39:32 -0500

    Added the remainder of the Python API
    Added the rest of the methods for the python API

--- docker/manager/utils.py
+++ docker/manager/utils.py
@@ -106,12 +106,11 @@ def convert_time(sTime=""):
 
 
 '''
-Starts a docker container with the provided image and options.
+Starts a docker container(s) with the provided image and options.
 
 Parameters: 
 container_name  =   (string) Name of the container
 image_name      =   (string) Name of the image to run
-quantity        =   (int) The number of containers to create
 links           =   (string list) A list of containers to link to
 host_mounts     =   (string:string dictionary) A dictionary linking paths to volumes
                     on the host computer to their mount destination in the container
@@ -120,10 +119,10 @@ custom_mounts   =   (string list) A list of paths for volumes to create and moun
 is_interactive  =   (bool) Specifies whether or not you want terminal access
 is_background   =   (bool) Specifies whether the container should run in the background
 
-Returns the response from the client start method
+Returns the response from the last initatied container
 '''
-def start_container(container_name, image_name, quantity, links, host_mounts,
-    external_mounts, custom_mounts, is_interactive, is_background):
+def start_container(container_name, image_name, quantity, links=True, host_mounts=True,
+    external_mounts=True, custom_mounts=True, is_interactive, is_background):
     
     # Request a docker client
     client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
@@ -161,8 +160,154 @@ def start_container(container_name, image_name, quantity, links, host_mounts,
             detach=is_background)
         containerID = container.get('Id')
 
-        # Start the container and return the response
+        # Start the container
         response = client.start(container=container.get('Id'), dns=dns_container_ip,
             publish_all_ports=True, volumes_from=external_mounts, links=links_dict, 
             binds=binds_dict)
+
+    # Return the last collected response
+    return response
+
+
+''' 
+Resumes a previously stopped docker container
+
+Parameters:
+container_name  =   (string) Name of the container
+
+Returns the response from the client start method
+'''
+def resume_container(container_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Resume the docker container and return the response
+    response = client.start(container=container_name)
+    return response
+
+
+'''
+Restarts a running docker container
+
+Parameters:
+container_name  =   (string) Name of the container
+
+Returns the response from the client restart method
+'''
+def restart_container(container_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Restart the docker container and return the response
+    response = client.restart(container=container_name, timeout=10)
+    return response
+
+
+'''
+Stops a running docker container
+
+Parameters:
+container_name  =   (string) Name of the container
+
+Returns the response from the client stop method
+'''
+def stop_container(container_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Stop the docker container and return the response
+    response = client.stop(container=container_name, timeout=10)
+    return response
+
+
+'''
+Removes a docker container
+
+Parameters:
+container_name  =   (string) Name of the container
+
+Returns the response from the client remove method 
+'''
+def remove_container(container_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Remove the docker container and return the response
+    response = client.remove_container(container=container_name, force=True)
+    return response
+
+
+'''
+Unlinks two docker containers
+
+Parameters:
+link_name  =   (string) Name of the container link
+               It should look something like: linker/base 
+
+Returns the response from the client remove (link) method 
+'''
+def unlink_containers(link_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Remove the docker container and return the response
+    response = client.remove_container(container=link_name, link=True)
+    return response
+
+
+'''
+Removes a docker image
+
+Parameters:
+image_name  =   (string) Name of the image
+
+Returns the response from the client remove image method
+'''
+def remove_image(image_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    """ TODO: Call script which removes git repo associated with the image as well """
+
+    # Remove the docker image and return the response
+    response = client.remove_image(image=image_name, force=True)
+    return response
+
+
+'''
+Branches a docker image
+
+Parameters:
+image_name  =   (string) Name of the image we're branching
+branch_name =   (string) Name of the new branch
+
+Returns the response from the client commit method
+'''
+def branch_image(image_name, branch_name):
+
+    # Request a docker client
+    client = docker.Client(base_url='unix://var/run/docker.sock', version='1.10', timeout=10)
+
+    # Create a container for the image (so we can use docker commit) and get its ID
+    # NOTE: The random number in the container is to reduce the likelihood of accidentally
+    # clobbering an existing container. Chosen by fair dice roll - guaranteed randomness
+    container = client.create_container(image=image_name, name='temp_container_7432983025')
+    containerID = container.get('Id')
+
+    # Create a 'copy' of the image (using docker commit)
+    response = client.commit(container=containerID, repository=branch_name, tag="branch")
+
+    """ TODO: Actually call David's copy git repo script """
+    # Call bash git copy image repo script
+
+    # Delete the container we made
+    client.remove_container(container=containerID, force=True)
+
+    # Return the response from the commit method
     return response
